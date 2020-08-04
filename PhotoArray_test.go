ccd22e1043d8a2502794395d0b5f221e7d408f6f
Mike Rapa <mike@mikerapa.com>
2020-08-03 22:33:08 -0400

    Added code for GenerateArray method and test

--- PhotoArray_test.go
+++ PhotoArray_test.go
@@ -1,8 +1,20 @@
 package PhotoArray
 
 import (
+	"fmt"
+	"math/rand"
+	"strings"
 	"testing"
+	"time"
 )
+
+func randomizedFilePath(pathTemplate string) string {
+	pathTemplate = strings.Replace(pathTemplate, "#", "%d", 1)
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	rndNum := r1.Intn(1000)
+	return fmt.Sprintf(pathTemplate, rndNum)
+}
 
 func TestNewPhotoArrayBuilder(t *testing.T) {
 	newBuilder := NewPhotoArrayBuilder()
@@ -73,6 +85,45 @@ func TestPhotoArrayBuilder_AddPhotoPaths(t *testing.T) {
 				t.Errorf("AddPhotoPath() error expected")
 			}
 
+		})
+	}
+}
+
+func TestPhotoArrayBuilder_GenerateArray(t *testing.T) {
+	tests := []struct {
+		name           string
+		PhotoHeight    int
+		PhotoWidth     int
+		RowLength      int
+		imageFilePaths []string
+		wantErr        bool
+	}{
+		{name: "no paths", PhotoHeight: 100, PhotoWidth: 100, RowLength: 10, imageFilePaths: []string{}, wantErr: true},
+		{name: "all 6 sample images", PhotoHeight: 100, PhotoWidth: 100, RowLength: 3, imageFilePaths: []string{"./testimages/soccer1.jpg", "./testimages/soccer2.jpg", "./testimages/soccer3.jpg", "./testimages/soccer4.jpg", "./testimages/soccer5.jpg", "./testimages/soccer6.jpg"}, wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pab := NewPhotoArrayBuilder()
+			pab.PhotoWidth = tt.PhotoWidth
+			pab.PhotoHeight = tt.PhotoHeight
+			pab.RowLength = tt.RowLength
+			pab.AddPhotoPaths(tt.imageFilePaths...)
+
+			// create an output path
+			outputPath := randomizedFilePath("./testoutputimages/outputimage#.jpg")
+			err := pab.GenerateArray(outputPath)
+
+			if err != nil {
+				if !tt.wantErr {
+					t.Errorf("GenerateArray() error = %v, wantErr %v", err, tt.wantErr)
+				}
+				return // if there's an error, do not check any other values
+			}
+
+			// check for the existence of the image, if there was no error
+			if !IsValidFilePath(outputPath) {
+				t.Errorf("file %s was not created.", outputPath)
+			}
 		})
 	}
 }
